2fc414bdc08c5ea2bd328078c1f50add9485a062
fionn-the-goose <ericksoncooperation@gmail.com>
2022-12-19 19:57:01 +0100

    firtst file startet.

---
+++ webshop-ddl.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE magic_shop;
+CREATE TABLE magic_karte(
+    KartenNr INT,
+    Name varchar(255),
+    Seltenheit varchar(255),
+    Sprache varchar(255),
+    Kartentext varchar(255),
+    Künstler varchar(255),
+    Manakosten varchar(255)
+);
